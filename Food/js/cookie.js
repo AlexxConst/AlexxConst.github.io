65e2455a9be0f4cec75360d7d4becdc75a04021d
Alexander Suslin <asuslin01@gmail.com>
2023-02-27 11:50:46 +0200

    add foodProject

--- Food/js/cookie.js
+++ Food/js/cookie.js
@@ -1,138 +1,126 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     // const cookieStorage = {
-//     //     getItem: (key) => {
-//     //         const cookies = document.cookie
-//     //             .split(';')
-//     //             .map((cookie) => cookie.split('='))
-//     //             .reduce(
-//     //                 (acc, [key, value]) => ({ ...acc, [key.trim()]: value }),
-//     //                 {}
-//     //             );
+window.addEventListener('DOMContentLoaded', () => {
+    const cookieStorage = {
+        getItem: (key) => {
+            const cookies = document.cookie
+                .split(';')
+                .map((cookie) => cookie.split('='))
+                .reduce(
+                    (acc, [key, value]) => ({ ...acc, [key.trim()]: value }),
+                    {}
+                );
 
-//     //         return cookies[key];
-//     //     },
-//     //     setItem: (key, value) => {
-//     //         document.cookie = `${key}=${value};expires=Sun, 16 Jul 3567 06:23:41 GMT`;
-//     //     },
-//     // };
+            return cookies[key];
+        },
+        setItem: (key, value) => {
+            document.cookie = `${key}=${value};expires=Sun, 16 Jul 3567 06:23:41 GMT`;
+        },
+    };
 
-//     // const storageType = cookieStorage;
-//     // const consentPropertyType = 'site_consent';
+    const storageType = cookieStorage;
+    const consentPropertyType = 'site_consent';
 
-//     // const hasConsented = () =>
-//     //     storageType.getItem(consentPropertyType) === 'true' ? true : false;
-//     // const toggleStorage = (prop) =>
-//     //     storageType.setItem(consentPropertyType, prop);
+    const hasConsented = () =>
+        storageType.getItem(consentPropertyType) === 'true' ? true : false;
+    const toggleStorage = (prop) =>
+        storageType.setItem(consentPropertyType, prop);
 
-//     // const popup = document.querySelector('.popup'),
-//     //     btnConfirm = document.querySelector('[data-confirm]'),
-//     //     btnCancel = document.querySelector('[data-cancel]');
+    const popup = document.querySelector('.popup'),
+        btnConfirm = document.querySelector('[data-confirm]'),
+        btnCancel = document.querySelector('[data-cancel]');
 
-//     // if (hasConsented()) {
-//     //     console.log('loading...');
-//     // } else {
-//     //     popup.classList.add('popup_active');
-//     // }
+    if (hasConsented()) {
+        // console.log('loading...');
+    } else {
+        popup.classList.add('popup_active');
+    }
 
-//     // btnConfirm.addEventListener('click', () => {
-//     //     toggleStorage(true);
-//     //     popup.classList.remove('popup_active');
-//     //     console.log('loading...');
-//     // });
+    btnConfirm.addEventListener('click', () => {
+        toggleStorage(true);
+        popup.classList.remove('popup_active');
+        // console.log('loading...');
+    });
 
-//     // btnCancel.addEventListener('click', () => {
-//     //     toggleStorage(false);
-//     //     popup.classList.remove('popup_active');
-//     // });
+    btnCancel.addEventListener('click', () => {
+        toggleStorage(false);
+        popup.classList.remove('popup_active');
+    });
 
-//     class CookieConsent {
-//         constructor({ popup, btnConfirm, btnCancel, activeClass = '' } = {}) {
-//             this.popup = document.querySelector(popup);
-//             this.btnConfirm = document.querySelector(btnConfirm);
-//             this.btnCancel = document.querySelector(btnCancel);
-//             this.activeClass = activeClass;
-//             this.consentPropertyType = 'site_consent';
-//         }
+    class CookieConsent {
+        constructor({ popup, btnConfirm, btnCancel, activeClass = '' } = {}) {
+            this.popup = document.querySelector(popup);
+            this.btnConfirm = document.querySelector(btnConfirm);
+            this.btnCancel = document.querySelector(btnCancel);
+            this.activeClass = activeClass;
+            this.consentPropertyType = 'site_consent';
+        }
 
-//         getItem = (key) => {
-//             const cookies = document.cookie
-//                 .split(';')
-//                 .map((cookie) => cookie.split('='))
-//                 .reduce(
-//                     (acc, [key, value]) => ({ ...acc, [key.trim()]: value }),
-//                     {}
-//                 );
-//             return cookies[key];
-//         };
+        getItem = (key) => {
+            const cookies = document.cookie
+                .split(';')
+                .map((cookie) => cookie.split('='))
+                .reduce(
+                    (acc, [key, value]) => ({ ...acc, [key.trim()]: value }),
+                    {}
+                );
+            return cookies[key];
+        };
 
-//         setItem = (key, value) => {
-//             document.cookie = `${key}=${value};expires=Sun, 16 Jul 3567 06:23:41 GMT`;
-//         };
+        setItem = (key, value) => {
+            document.cookie = `${key}=${value};expires=Sun, 16 Jul 3567 06:23:41 GMT`;
+        };
 
-//         hasConsented = () => {
-//             if (this.getItem(this.consentPropertyType) === 'true') {
-//                 return true;
-//             } else {
-//                 return false;
-//             }
-//         };
+        hasConsented = () => {
+            if (this.getItem(this.consentPropertyType) === 'true') {
+                return true;
+            } else {
+                return false;
+            }
+        };
 
-//         changeStatus = (prop) => {
-//             this.setItem(this.consentPropertyType, prop);
-//             if (this.hasConsented()) {
-//                 //Подписка
-//                 myScripts();
-//             }
-//         };
+        changeStatus = (prop) => {
+            this.setItem(this.consentPropertyType, prop);
+            if (this.hasConsented()) {
+                //Подписка
+                myScripts();
+            }
+        };
 
-//         bindTriggers = () => {
-//             this.btnConfirm.addEventListener('click', () => {
-//                 this.changeStatus(true);
-//                 this.popup.classList.remove('this.activeClass');
-//             });
+        bindTriggers = () => {
+            this.btnConfirm.addEventListener('click', () => {
+                this.changeStatus(true);
+                this.popup.classList.remove('this.activeClass');
+            });
 
-//             this.btnCancel.addEventListener('click', () => {
-//                 this.changeStatus(false);
-//                 this.popup.classList.remove('this.activeClass');
-//             });
-//         };
+            this.btnCancel.addEventListener('click', () => {
+                this.changeStatus(false);
+                this.popup.classList.remove('this.activeClass');
+            });
+        };
 
-//         init = () => {
-//             try {
-//                 if (this.hasConsented()) {
-//                     myScripts();
-//                 } else {
-//                     this.popup.classList.add(this.activeClass);
-//                 }
+        init = () => {
+            try {
+                if (this.hasConsented()) {
+                    myScripts();
+                } else {
+                    this.popup.classList.add(this.activeClass);
+                }
 
-//                 this.bindTriggers();
-//             } catch (e) {
-//                 console.error('Переданы не все данные');
-//             }
-//         };
-//     }
+                this.bindTriggers();
+            } catch (e) {
+                console.error('Переданы не все данные');
+            }
+        };
+    }
 
-//     new CookieConsent({
-//         activeClass: 'popup_active',
-//         popup: '.popup',
-//         btnConfirm: '[data-confirm]',
-//         btnCancel: '[data-cancel]',
-//     }).init();
+    new CookieConsent({
+        activeClass: 'popup_active',
+        popup: '.popup',
+        btnConfirm: '[data-confirm]',
+        btnCancel: '[data-cancel]',
+    }).init();
 
-//     function myScripts() {
-//         console.log('Loading');
-//     }
-// });
+    function myScripts() {
+        // console.log('Loading');
+    }
+});
 
-//=====================
-
-let user = {
-    name: 'John',
-    age: 30,
-};
-
-user.sayHi = function () {
-    console.log('Привет!');
-};
-
-user.sayHi();

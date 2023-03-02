1974455d85eb7fc1ff1b8e2f40cce3d6a9d00e5a
Alexander Suslin <asuslin01@gmail.com>
2023-03-02 08:57:20 +0200

    add smooth bar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-const hamburger = document.querySelector(".hamburger"),
-  menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
 
-hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
+const closeMenu = () => {
+    menu.classList.remove('active');
+};
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
 });
 
-closeElem.addEventListener("click", () => {
-  menu.classList.remove("active");
+closeElem.addEventListener('click', closeMenu);
+
+menu.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu__overlay')) {
+        closeMenu();
+    }
 });
 
-const counters = document.querySelectorAll(".skills__ratings-counter"),
-  lines = document.querySelectorAll(".skills__ratings-line span");
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach((item, i) => {
-  lines[i].style.width = item.innerHTML;
+    lines[i].style.width = item.innerHTML;
 });
